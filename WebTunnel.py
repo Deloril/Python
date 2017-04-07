b3b9bf63c0bc2c96d50fbb61899cc275db1a2b05
Luke Pearson <Luke.Pearson3@psba.qld.gov.au>
2017-04-07 21:10:22 +1000

    Derp

--- WebTunnel.py
+++ WebTunnel.py
@@ -5,8 +5,8 @@ QR = "P75hn0VCl8sU969U80My"
 
 #easyctf{y0u_sh0uld_b3_t1r3d_tr4v3ll1ng_all_th1s_w4y!!!!!}
 while 1==1:
-	print ("Requesting: " + baseSite + QR)
-	proc = urllib2.urlopen(baseSite + QR)
-	data = proc.read()
-	data = data.split("data\":\"")
-	QR = data[1].split("\",")[0]
+    print ("Requesting: " + baseSite + QR)
+    proc = urllib2.urlopen(baseSite + QR)
+    data = proc.read()
+    data = data.split("data\":\"")
+    QR = data[1].split("\",")[0]
